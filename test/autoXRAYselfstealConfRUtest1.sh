5cbdfa39de308d903dad0c78af6280d2ce1473c4
xVRVx <63975481+xVRVx@users.noreply.github.com>
2025-11-27 10:52:52 +0300

    Add files via upload

--- test/autoXRAYselfstealConfRUtest1.sh
+++ test/autoXRAYselfstealConfRUtest1.sh
@@ -207,14 +207,13 @@ cat << 'EOF' | envsubst > "$SCRIPT_DIR/config.json"
   },
   "inbounds": [
     {
-      "tag": "VtcpRself",
+      "tag": "VxhttpRself",
       "port": 443,
       "listen": "0.0.0.0",
       "protocol": "vless",
       "settings": {
         "clients": [
           {
-            "flow": "xtls-rprx-vision",
             "id": "${xray_uuid_vrv}"
           }
         ],
@@ -229,7 +228,11 @@ cat << 'EOF' | envsubst > "$SCRIPT_DIR/config.json"
         ]
       },
       "streamSettings": {
-        "network": "raw",
+        "network": "xhttp",
+        "xhttpSettings": {
+          "mode": "auto",
+		  "path": "/xvrvxtt"
+        },
         "security": "reality",
         "realitySettings": {
           "show": false,
@@ -256,14 +259,15 @@ cat << 'EOF' | envsubst > "$SCRIPT_DIR/config.json"
         }
       }
     },
-    {
-      "tag": "VxhttpRself",
+{
+      "tag": "VtcpRself",
       "port": 8443,
       "listen": "0.0.0.0",
       "protocol": "vless",
       "settings": {
         "clients": [
           {
+            "flow": "xtls-rprx-vision",
             "id": "${xray_uuid_vrv}"
           }
         ],
@@ -278,11 +282,7 @@ cat << 'EOF' | envsubst > "$SCRIPT_DIR/config.json"
         ]
       },
       "streamSettings": {
-        "network": "xhttp",
-        "xhttpSettings": {
-          "mode": "auto",
-		  "path": "/xvrvxtt"
-        },
+        "network": "raw",
         "security": "reality",
         "realitySettings": {
           "show": false,
@@ -503,7 +503,6 @@ cat << 'EOF' | envsubst > "$WEB_PATH/$path_subpage.json"
             "users": [
               {
                 "id": "${xray_uuid_vrv}",
-                "flow": "xtls-rprx-vision",
                 "encryption": "none",
                 "security": "auto"
               }
@@ -512,9 +511,10 @@ cat << 'EOF' | envsubst > "$WEB_PATH/$path_subpage.json"
         ]
       },
       "streamSettings": {
-        "network": "raw",
-        "rawSettings": {
-          "acceptProxyProtocol": false
+        "network": "xhttp",
+        "xhttpSettings": {
+          "mode": "auto",
+		  "path": "/xvrvxtt"
         },
         "security": "reality",
         "realitySettings": {
@@ -537,7 +537,7 @@ cat << 'EOF' | envsubst > "$WEB_PATH/$path_subpage.json"
       "protocol": "blackhole"
     }
   ],
-  "remarks": "🇧🇩 VlRawRtyXtls - autoXRAY"
+  "remarks": "🇧🇩 VlXhttpRty - autoXRAY"
 },
 {
   "log": {
@@ -662,6 +662,7 @@ cat << 'EOF' | envsubst > "$WEB_PATH/$path_subpage.json"
             "users": [
               {
                 "id": "${xray_uuid_vrv}",
+                "flow": "xtls-rprx-vision",
                 "encryption": "none",
                 "security": "auto"
               }
@@ -670,14 +671,7 @@ cat << 'EOF' | envsubst > "$WEB_PATH/$path_subpage.json"
         ]
       },
       "streamSettings": {
-        "network": "xhttp",
-        "xhttpSettings": {
-          "mode": "auto",
-		  "path": "/xvrvxtt"
-        },
-        "rawSettings": {
-          "acceptProxyProtocol": false
-        },
+        "network": "raw",
         "security": "reality",
         "realitySettings": {
           "show": false,
@@ -699,7 +693,7 @@ cat << 'EOF' | envsubst > "$WEB_PATH/$path_subpage.json"
       "protocol": "blackhole"
     }
   ],
-  "remarks": "🇧🇩 VlXhttpRty - autoXRAY"
+  "remarks": "🇧🇩 VlRawRtyXtls - autoXRAY"
 },
 {
   "log": {
@@ -851,9 +845,9 @@ echo -e "Готово!\n"
 subPageLink="https://$DOMAIN/$path_subpage.json"
 
 # Формирование ссылок
-link1="vless://${xray_uuid_vrv}@$DOMAIN:443?security=reality&type=tcp&headerType=&path=&host=&flow=xtls-rprx-vision&sni=$DOMAIN&fp=chrome&pbk=${xray_publicKey_vrv}&sid=${xray_shortIds_vrv}&spx=%2F#VlRawRtyXtls"
+link1="vless://${xray_uuid_vrv}@$DOMAIN:443?security=reality&type=xhttp&headerType=&path=xvrvxtt&host=&mode=auto&sni=$DOMAIN&fp=chrome&pbk=${xray_publicKey_vrv}&sid=${xray_shortIds_vrv}&spx=%2F#VlXhttpRty"
 
-link2="vless://${xray_uuid_vrv}@$DOMAIN:8443?security=reality&type=xhttp&headerType=&path=xvrvxtt&host=&mode=auto&sni=$DOMAIN&fp=chrome&pbk=${xray_publicKey_vrv}&sid=${xray_shortIds_vrv}&spx=%2F#VlXhttpRty"
+link2="vless://${xray_uuid_vrv}@$DOMAIN:8443?security=reality&type=tcp&headerType=&path=&host=&flow=xtls-rprx-vision&sni=$DOMAIN&fp=chrome&pbk=${xray_publicKey_vrv}&sid=${xray_shortIds_vrv}&spx=%2F#VlRawRtyXtls"
 
 ENCODED_STRING=$(echo -n "chacha20-ietf-poly1305:${xray_sspasw_vrv}" | base64)
 link3="ss://$ENCODED_STRING@${ipserv}:10443#ShadowS"
@@ -863,15 +857,16 @@ configListLink="https://$DOMAIN/$path_subpage.html"
 # Создаем html файл с конфигами
 cat > "$WEB_PATH/$path_subpage.html" <<EOF
 <!DOCTYPE html><html lang="ru"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1.0"><meta name="robots" content="noindex,nofollow,noarchive,nosnippet,noimageindex"><meta name="googlebot" content="noindex,nofollow,noarchive,nosnippet,noimageindex"><meta name="bingbot" content="noindex,nofollow,noarchive,nosnippet,noimageindex"><title>Xray Configs</title><style>body{font-family:monospace;background:#121212;color:#e0e0e0;padding:20px;max-width:800px;margin:0 auto}h3{color:#82aaff;border-bottom:1px solid #333;padding-bottom:10px;margin-top:30px}.box{background:#1e1e1e;padding:15px;border-radius:8px;word-break:break-all;border:1px solid #333;margin-bottom:10px}.box a{color:#c3e88d;text-decoration:none;display:block;margin-top:10px;font-weight:700}.box a:hover{text-decoration:underline}.btn-group{display:flex;flex-wrap:wrap;gap:15px;margin-top:25px}.btn{flex:1;min-width:250px;background-color:#2c2c2c;color:#c3e88d;border:1px solid #c3e88d;padding:15px;text-align:center;border-radius:8px;text-decoration:none;font-weight:700;transition:all 0.3s ease;display:flex;align-items:center;justify-content:center}.btn:hover{background-color:#c3e88d;color:#121212;cursor:pointer;box-shadow:0 0 10px rgba(195,232,141,.3)}.btn.download{border-color:#82aaff;color:#82aaff}.btn.download:hover{background-color:#82aaff;color:#121212;box-shadow:0 0 10px rgba(130,170,255,.3)}</style></head>
-<body><h3>🚀 VLESS RAW Reality Vision - для роутера</h3><div class="box">$link1</div><h3>🛸 VLESS XHTTP Reality - новая разработка</h3><div class="box">$link2</div><h3>🛡️ Shadowsocks - простой и быстрый</h3><div class="box">$link3</div><h3>📂 Ссылка на подписку (готовый конфиг клиента с роутингом)</h3><div class="box"><a href="$subPageLink" target="_blank">$subPageLink</a></div><h3>📱 Приложение HAPP (Windows/Android/iOS/MAC/Linux)</h3>
+<body><h3>🛸 VLESS XHTTP Reality - конфиг для роутера</h3><div class="box">$link1</div><h3>🚀 VLESS RAW Reality Vision</h3><div class="box">$link2</div><h3>🛡️ Shadowsocks - простой и быстрый</h3><div class="box">$link3</div><h3>📂 Ссылка на подписку (готовый конфиг клиента с роутингом)</h3><div class="box"><a href="$subPageLink" target="_blank">$subPageLink</a></div><h3>📱 Приложение HAPP (Windows/Android/iOS/MAC/Linux)</h3>
 <p>Маршрутизацию нужно выключить, она тут встроенная. По умолчанию она выключена - включатся, если вы пользовались сторонними сервисами.</p><div class="btn-group"><a href="happ://add/$subPageLink" class="btn">⚡ Автодобавление в HAPP</a><a href="https://www.happ.su/main/ru" target="_blank" class="btn download">⬇️ Скачать HAPP</a></div></body></html>
 EOF
 
 echo -e "
-Ваш конфиг для роутера vless raw reality xtls:
+
+Ваш конфиг для роутера vless xhttp reality:
 $link1
 
-Ваш конфиг vless xhttp reality:
+Ваш конфиг vless raw reality xtls:
 $link2
 
 Ваш конфиг Shadowsocks chacha20-ietf-poly1305:
