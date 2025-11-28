f54ceb44740ffce45e777ec3649b248085798091
xVRVx <63975481+xVRVx@users.noreply.github.com>
2025-11-28 16:41:48 +0300

    Add files via upload

--- test/autoXRAYselfConfRUxhttp222.sh
+++ test/autoXRAYselfConfRUxhttp222.sh
@@ -178,7 +178,8 @@ verify_mldsa65=$(echo "$key_mldsa65" | awk -F': ' '/Verify/ {print $2}')
 
 xray_shortIds_vrv=$(openssl rand -hex 8)
 
-xray_sspasw_vrv=$(openssl rand -base64 15 | tr -dc 'A-Za-z0-9' | head -c 20)
+# xray_sspasw_vrv=$(openssl rand -base64 15 | tr -dc 'A-Za-z0-9' | head -c 20)
+xray_sspasw_vrv=$(openssl rand -base64 32)
 
 path_subpage=$(openssl rand -base64 15 | tr -dc 'A-Za-z0-9' | head -c 20)
 
@@ -211,7 +212,7 @@ cat << 'EOF' | envsubst > "$SCRIPT_DIR/config.json"
     {
       "tag": "VxhttpRself",
       "port": 3333,
-      "listen": "0.0.0.0",
+      "listen": "127.0.0.1",
       "protocol": "vless",
       "settings": {
         "clients": [
@@ -302,7 +303,7 @@ cat << 'EOF' | envsubst > "$SCRIPT_DIR/config.json"
         "clients": [
           {
             "password": "${xray_sspasw_vrv}",
-            "method": "chacha20-ietf-poly1305"
+            "method": "2022-blake3-chacha20-poly1305"
           }
         ]
       },
@@ -315,7 +316,7 @@ cat << 'EOF' | envsubst > "$SCRIPT_DIR/config.json"
         ]
       },
       "streamSettings": {
-        "network": "raw"
+        "network": "tcp,udp"
       }
     }
   ],
@@ -520,7 +521,7 @@ cat << 'EOF' | envsubst > "$WEB_PATH/$path_subpage.json"
       "protocol": "blackhole"
     }
   ],
-  "remarks": "🇧🇩 vlessXHTTPreality"
+  "remarks": "🇧🇩 vlessXHTTPreality - autoXRAY"
 },
 {
   "log": {
@@ -675,7 +676,7 @@ cat << 'EOF' | envsubst > "$WEB_PATH/$path_subpage.json"
       "protocol": "blackhole"
     }
   ],
-  "remarks": "🇧🇩 VlessRAWrealityXTLS"
+  "remarks": "🇧🇩 VlessRAWrealityXTLS - autoXRAY"
 },
 {
   "log": {
@@ -796,7 +797,7 @@ cat << 'EOF' | envsubst > "$WEB_PATH/$path_subpage.json"
         "servers": [
           {
             "port": 8443,
-            "method": "chacha20-ietf-poly1305",
+            "method": "2022-blake3-chacha20-poly1305",
             "address": "$DOMAIN",
             "password": "${xray_sspasw_vrv}"
           }
@@ -812,7 +813,7 @@ cat << 'EOF' | envsubst > "$WEB_PATH/$path_subpage.json"
       "protocol": "blackhole"
     }
   ],
-  "remarks": "🇧🇩 ShadowScha20 - autoXRAY"
+  "remarks": "🇧🇩 ShadowS2020blake3 - autoXRAY"
 }
 ]
 
@@ -827,19 +828,19 @@ echo -e "Готово!\n"
 subPageLink="https://$DOMAIN/$path_subpage.json"
 
 # Формирование ссылок
-link1="vless://${xray_uuid_vrv}@$DOMAIN:443?security=reality&type=xhttp&headerType=&path=%2F$path_xhttp&host=&mode=auto&sni=$DOMAIN&fp=chrome&pbk=${xray_publicKey_vrv}&sid=${xray_shortIds_vrv}&spx=%2F#VlXhttpRty"
+link1="vless://${xray_uuid_vrv}@$DOMAIN:443?security=reality&type=xhttp&headerType=&path=%2F$path_xhttp&host=&mode=auto&sni=$DOMAIN&fp=chrome&pbk=${xray_publicKey_vrv}&sid=${xray_shortIds_vrv}&spx=%2F#vlessXHTTPreality-autoXRAY"
 
-link2="vless://${xray_uuid_vrv}@$DOMAIN:443?security=reality&type=tcp&headerType=&path=&host=&flow=xtls-rprx-vision&sni=$DOMAIN&fp=chrome&pbk=${xray_publicKey_vrv}&sid=${xray_shortIds_vrv}&spx=%2F#VlRawRtyXtls"
+link2="vless://${xray_uuid_vrv}@$DOMAIN:443?security=reality&type=tcp&headerType=&path=&host=&flow=xtls-rprx-vision&sni=$DOMAIN&fp=chrome&pbk=${xray_publicKey_vrv}&sid=${xray_shortIds_vrv}&spx=%2F#vlessRAWrealityXTLS-autoXRAY"
 
-ENCODED_STRING=$(echo -n "chacha20-ietf-poly1305:${xray_sspasw_vrv}" | base64)
-link3="ss://$ENCODED_STRING@${ipserv}:8443#ShadowS"
+ENCODED_STRING=$(echo -n "2022-blake3-chacha20-poly1305:${xray_sspasw_vrv}" | base64)
+link3="ss://$ENCODED_STRING@${ipserv}:8443#Shadowsocks2020-autoXRAY"
 
 configListLink="https://$DOMAIN/$path_subpage.html"
 
 # Создаем html файл с конфигами
 cat > "$WEB_PATH/$path_subpage.html" <<EOF
 <!DOCTYPE html><html lang="ru"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1.0"><meta name="robots" content="noindex,nofollow,noarchive,nosnippet,noimageindex"><meta name="googlebot" content="noindex,nofollow,noarchive,nosnippet,noimageindex"><meta name="bingbot" content="noindex,nofollow,noarchive,nosnippet,noimageindex"><title>AutoXRAY configs</title><style>body{font-family:monospace;background:#121212;color:#e0e0e0;padding:20px;max-width:800px;margin:0 auto}h3{color:#82aaff;border-bottom:1px solid #333;padding-bottom:10px;margin-top:30px}.box{background:#1e1e1e;padding:15px;border-radius:8px;word-break:break-all;border:1px solid #333;margin-bottom:10px}.box a{color:#c3e88d;text-decoration:none;display:block;margin-top:10px;font-weight:700}.box a:hover{text-decoration:underline}.btn-group{display:flex;flex-wrap:wrap;gap:15px;margin-top:25px}.btn{flex:1;min-width:250px;background-color:#2c2c2c;color:#c3e88d;border:1px solid #c3e88d;padding:15px;text-align:center;border-radius:8px;text-decoration:none;font-weight:700;transition:all 0.3s ease;display:flex;align-items:center;justify-content:center}.btn:hover{background-color:#c3e88d;color:#121212;cursor:pointer;box-shadow:0 0 10px rgba(195,232,141,.3)}.btn.download{border-color:#82aaff;color:#82aaff}.btn.download:hover{background-color:#82aaff;color:#121212;box-shadow:0 0 10px rgba(130,170,255,.3)}</style></head>
-<body><h3>🛸 VLESS XHTTP Reality - конфиг для роутера</h3><div class="box">$link1</div><h3>🚀 VLESS RAW Reality xtls-rprx-vision</h3><div class="box">$link2</div><h3>🛡️ Shadowsocks - простой и быстрый</h3><div class="box">$link3</div><h3>📂 Ссылка на подписку (готовый конфиг клиента с роутингом)</h3><div class="box">$subPageLink</div><h3>📱 Приложение HAPP (Windows/Android/iOS/MAC/Linux)</h3>
+<body><h3>🛸 VLESS XHTTP Reality - конфиг для роутера</h3><div class="box">$link1</div><h3>🚀 VLESS RAW Reality xtls-rprx-vision</h3><div class="box">$link2</div><h3>🛡️ Shadowsocks2020blake3 - новый и быстрый</h3><div class="box">$link3</div><h3>📂 Ссылка на подписку (готовый конфиг клиента с роутингом)</h3><div class="box">$subPageLink</div><h3>📱 Приложение HAPP (Windows/Android/iOS/MAC/Linux)</h3>
 <p>Маршрутизацию нужно выключить, она тут встроенная. По умолчанию она выключена - включатся, если вы пользовались сторонними сервисами.</p><div class="btn-group"><a href="happ://add/$subPageLink" class="btn">⚡ Автодобавление в HAPP</a><a href="https://www.happ.su/main/ru" target="_blank" class="btn download">⬇️ Скачать HAPP</a></div></body></html>
 EOF
 
@@ -851,7 +852,7 @@ $link1
 Ваш конфиг vless raw reality xtls:
 $link2
 
-Ваш конфиг Shadowsocks chacha20-ietf-poly1305:
+Ваш конфиг Shadowsocks 2022-blake3-chacha20-poly1305:
 $link3
 
 Ваша страничка подписки:
