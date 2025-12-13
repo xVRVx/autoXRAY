2382cc71e1ed4f48df0e4f55a6cb405bb113372c
xVRVx <63975481+xVRVx@users.noreply.github.com>
2025-12-13 17:46:50 +0300

    Add files via upload

--- test/autoXRAYselfTLS2.sh
+++ test/autoXRAYselfTLS2.sh
@@ -272,6 +272,11 @@ cat << 'EOF' | envsubst > "$SCRIPT_DIR/config.json"
               "certificateFile": "/var/lib/xray/cert/fullchain.pem",
               "keyFile": "/var/lib/xray/cert/privkey.pem"
             }
+          ],
+          "minVersion": "1.2",
+          "cipherSuites": "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256:TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256:TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384:TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384:TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256:TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", 
+          "alpn": [
+            "h2", "http/1.1"
           ]
         }
       },
@@ -795,7 +800,7 @@ cat > "$WEB_PATH/$path_subpage.html" <<EOF
 EOF
 
 echo -e "
-test-111
+test-333
 
 Ваша json страничка подписки:
 \033[32m$subPageLink\033[0m
